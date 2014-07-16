d4a10dc4306df4ff13651f4a6a0454b661ae0d30
Nathan Smith <nathansmithdeveloper@gmail.com>
2014-07-16 14:59:02 +1200

    Create initNanoScroller.js

---
+++ initNanoScroller.js
@@ -0,0 +1,40 @@
+$.fn.initNanoScroller = function() {
+
+	var $scrollBody = $(this),
+		$scrollList = $scrollBody.children(),
+		$listChildren = $scrollList.children(),
+		totalHeight = 0,
+		$newArticleButton = $('<a href="#" class="newArticleButton">Back to top</a>');
+
+	$scrollBody.addClass('nano');
+	$scrollList.addClass('content');
+
+	var $nano = $('.nano');
+
+	$listChildren.each(function(){
+		totalHeight += $(this).outerHeight();
+	});
+
+	if(totalHeight > $scrollBody.height()){
+	  $nano.nanoScroller({ preventPageScrolling: true });
+
+		$nano.bind("scrollend", function(){
+		  	$newArticleButton.show().insertBefore($scrollList);
+		});
+
+		$newArticleButton.on('click', function(e){
+			e.preventDefault();
+			$nano.nanoScroller({ scroll: 'top' });
+			$newArticleButton.slideUp(100);
+		});
+
+		$nano.bind("scrolltop", function(){
+		    $newArticleButton.slideUp(100);
+		});
+
+	} else {
+		$nano.nanoScroller({ stop: true });
+		$scrollBody.height(totalHeight);
+	}
+
+}
